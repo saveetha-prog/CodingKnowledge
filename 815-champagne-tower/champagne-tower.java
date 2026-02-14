21941c97fc98adc9e2eb79496485e838e0cffb3e
Saveetha.A <saveethaaarumugam@gmail.com>
2026-02-14 19:26:09 +0530

    Time: 2 ms (98.59%) | Memory: 47.9 MB (84.45%) - LeetSync

---
+++ 815-champagne-tower/champagne-tower.java
@@ -0,0 +1,21 @@
+class Solution {
+    public double champagneTower(int poured, int query_row, int query_glass) {
+        
+        double[][] dp = new double[query_row + 2][query_row + 2];
+        dp[0][0] = poured;
+        
+        for (int i = 0; i <= query_row; i++) {
+            for (int j = 0; j <= i; j++) {
+                
+                if (dp[i][j] > 1) {
+                    double overflow = (dp[i][j] - 1) / 2.0;
+                    dp[i + 1][j] += overflow;
+                    dp[i + 1][j + 1] += overflow;
+                    dp[i][j] = 1;  
+                }
+            }
+        }
+        
+        return dp[query_row][query_glass];
+    }
+}
